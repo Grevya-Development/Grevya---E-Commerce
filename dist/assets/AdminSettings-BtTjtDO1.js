37eb26cd1017cd2d428567bddc9d1ea2df6fcff4
yashBabu2002 <yashwanthbabu2002@gmail.com>
2026-06-11 11:18:34 +0530

    fix: bug fixed

---
+++ dist/assets/AdminSettings-BtTjtDO1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BZEZQa2s.js";import{A as e}from"./AdminLayout-Q8xRyvOs.js";function i(){return t.jsxs(e,{children:[t.jsx("h1",{className:"text-3xl font-bold text-green-900",children:"Settings"}),t.jsx("p",{className:"text-gray-600 mt-2",children:"Admin settings panel"})]})}export{i as default};
---
+++ dist/assets/package-check-DYUIeNYD.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-BZEZQa2s.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("BadgeCheck",[["path",{d:"M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z",key:"3c2336"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=e("PackageCheck",[["path",{d:"m16 16 2 2 4-4",key:"gfu2re"}],["path",{d:"M21 10V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l2-1.14",key:"e7tb2h"}],["path",{d:"m7.5 4.27 9 5.15",key:"1c824w"}],["polyline",{points:"3.29 7 12 12 20.71 7",key:"ousv84"}],["line",{x1:"12",x2:"12",y1:"22",y2:"12",key:"a4e8g8"}]]);export{c as B,k as P};
